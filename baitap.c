5ca06522e0e03d50ddabefe75898cea7be21bf05
Đinh Quang Anh <91303593+DQuangAnh@users.noreply.github.com>
2021-09-24 12:37:41 +0700

    Add files via upload

---
+++ baitap.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	printf("Dau giuong anh trang roi\n");
+	printf("Ngo mat dat phu suong\n");
+	printf("Ngang dau nhin trang sang\n");
+	printf("Cui dau nho co huong\n");
+	return 0;
+}
